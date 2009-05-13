64501df7f5d684ea375f894bf4ccd5a7060cfafb
andres.delikat <andres.delikat@d9c9f5a8-3e68-11de-97fb-aff7d2a6cc54>
2009-05-13 15:03:31 +0000

    Moved Read only toggle Hotkey to a Movie page

--- trunk/src/windows/hotkey.h
+++ trunk/src/windows/hotkey.h
@@ -32,6 +32,7 @@
 
 enum HotkeyPage {
 	HOTKEY_PAGE_MAIN=0,
+	HOTKEY_PAGE_MOVIE,
 	HOTKEY_PAGE_STATE,
 	HOTKEY_PAGE_STATE_SLOTS,
 	HOTKEY_PAGE_TURBO,
@@ -40,6 +41,7 @@ enum HotkeyPage {
 
 static LPCTSTR hotkeyPageTitle[] = {
 	_T("Main"),
+	_T("Movie"),
 	_T("Savestates"),
 	_T("Savestate Slots"),
 	_T("Turbo"),
