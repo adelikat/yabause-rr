0415a11f057f0ea5481d2d8a2aca56a27e59973f
pault414 <pault414@d9c9f5a8-3e68-11de-97fb-aff7d2a6cc54>
2009-05-26 09:27:35 +0000

    more complete resets.  this fixes most, if not all of the desync problems

--- trunk/src/windows/moviewin.cpp
+++ trunk/src/windows/moviewin.cpp
@@ -523,7 +523,8 @@ void FCEUI_LoadMovie(const char *fname, bool _read_only, bool tasedit, int _paus
 //	extern bool _HACK_DONT_STOPMOVIE;
 //	_HACK_DONT_STOPMOVIE = true;
 //	NDS_Reset();
-	YabauseReset();
+	HardResetGame();
+//	YabauseReset();
 //	_HACK_DONT_STOPMOVIE = false;
 	////WE NEED TO LOAD A SAVESTATE
 	//if(currMovieData.savestate.size() != 0)
@@ -587,7 +588,8 @@ static void openRecordingMovie(const char* fname)
 //	extern bool _HACK_DONT_STOPMOVIE;
 //	_HACK_DONT_STOPMOVIE = true;
 //	NDS_Reset();
-	YabauseReset();
+	HardResetGame();
+	//YabauseReset();
 //	_HACK_DONT_STOPMOVIE = false;
 
 	//todo ?
